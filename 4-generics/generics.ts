747ffa7cbeddd5c9e1ebbf319fa554677cdfecfc
jigyeong <wlrud0126@gmail.com>
2022-09-20 21:39:10 +0900

    4-generics

---
+++ 4-generics/generics.ts
@@ -0,0 +1,16 @@
+// 문제점
+function loggingOnlyTwoType(text: string | number){
+    console.log(text);
+    return text;
+}
+loggingOnlyTwoType(text).split(''); // number type 일 수도 있어 Error 발생
+
+// generics
+function logging<T>(text: T):T {
+    console.log(text);
+    return text;
+}
+
+logging<string>("Hello");
+logging<number>(1);
+logging<boolean>(true);
