0f76010132866643be0802d26878473c5a4638f7
jigyeong <wlrud0126@gmail.com>
2022-09-27 11:26:29 +0900

    4-generics : Generic exercise project

---
+++ 4-generics/practice/src/index.ts
@@ -0,0 +1,98 @@
+interface PhoneNumberDictionary {
+    [phone: string]: {
+      num: number;
+    };
+  }
+  
+  interface Contact {
+    name: string;
+    address: string;
+    phones: PhoneNumberDictionary;
+  }
+  
+  // api
+  function fetchContacts():Promise<Contact[]> {
+    const contacts:Contact[] = [
+      {
+        name: 'Tony',
+        address: 'Malibu',
+        phones: {
+          home: {
+            num: 11122223333,
+          },
+          office: {
+            num: 44455556666,
+          },
+        },
+      },
+      {
+        name: 'Banner',
+        address: 'New York',
+        phones: {
+          home: {
+            num: 77788889999,
+          },
+        },
+      },
+      {
+        name: '마동석',
+        address: '서울시 강남구',
+        phones: {
+          home: {
+            num: 213423452,
+          },
+          studio: {
+            num: 314882045,
+          },
+        },
+      },
+    ];
+    return new Promise(resolve => {
+      setTimeout(() => resolve(contacts), 2000);
+    });
+  }
+  
+  // main
+  class AddressBook {
+    private contacts:Contact[];
+  
+    constructor() {
+        this.contacts = [];
+        this.fetchData();
+    }
+  
+    fetchData() {
+      fetchContacts().then(response => {
+        this.contacts = response;
+      });
+    }
+  
+    findContactByName(name: string) {
+      return this.contacts.filter(contact => contact.name === name);
+    }
+  
+    findContactByAddress(address: string) {
+      return this.contacts.filter(contact => contact.address === address);
+    }
+  
+    findContactByPhone(phoneNumber: number, phoneType: string) {
+      return this.contacts.filter(
+        contact => contact.phones[phoneType].num === phoneNumber
+      );
+    }
+  
+    addContact(contact:Contact) {
+      this.contacts.push(contact);
+    }
+  
+    displayListByName() {
+      return this.contacts.map(contact => contact.name);
+    }
+  
+    displayListByAddress() {
+      return this.contacts.map(contact => contact.address);
+    }
+    /* ------------------------------------------------ */
+  }
+  
+  new AddressBook();
