2dc2be4d6162801cae5c8968c36d505bc82e44ba
jigyeong <wlrud0126@gmail.com>
2022-09-29 10:59:32 +0900

    5-type_sessential : 타입추론

---
+++ 5-type_essential/type-inference.ts
@@ -0,0 +1,32 @@
+let a = 'abc';
+
+function getB(b=10){
+    var c = 'hi';
+    return b+c;
+}
+
+interface Dropdown<T>{
+    value: T;
+    title: string;
+}
+
+let shoppingItem: Dropdown<string> = {
+    value: 'val',
+    title: 'tit'
+}
+
+interface DetailedDropdown<K> extends Dropdown<K>{
+    description: string;
+    tag: K;
+}
+
+
+let detailedItem:DetailedDropdown<number> = {
+    description: "",
+    tag: 0,
+    value: 0,
+    title: ""
+}
+
+// (string | number | boolean)[]
+let arr = [1,2,true,'string']
